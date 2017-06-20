cd18e2abcf82c6517726b459764aec4952f96b7e
Ercan Duman <ercanduman30@gmail.com>
2017-06-20 09:49:54 +0300

    // val > final (immutable, unchangeable, read-only) variables  // var > changable variables | ;(semi-colons) are free, not necessary in Kotlin  // Variables can be defined without declaring its type  // Strings can be used as string interpolation which means you can use variables inside a text

--- app/src/main/java/ercanduman/kotlinnotes/MainActivity.kt
+++ app/src/main/java/ercanduman/kotlinnotes/MainActivity.kt
@@ -10,10 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //val > final (immutable, unchangeable, read-only) variables
+        val name: String = "Ercan";
+
+        // var > changable variables | ;(semi-colons) are free, not necessary in Kotlin
+        var age: Int = 27;
+
+        //variables can be defined without declaring its type
+        val username = "ercanduman" // this is a String variable
+
+        // Strings can be used as string interpolation
+        // which means you can use variables inside a text as shown below
+        var myText = name + " is $age years old"
+
+
         //setting test to textviews
         textview_text.setText("First Kotlin app")
 
         // button clicks handled easily and very readable
-        button.setOnClickListener { textview_text.setText("Button clicked!") }
+        button.setOnClickListener { textview_text.setText(myText) }
     }
 }
